4dd82bc4bd0696a1dd59089100aaf6f425ad6ef1
Erathriel <geoffreynoro@orange.fr>
2018-05-16 10:52:55 +0200

    Create ascenceur.c

---
+++ ascenceur.c
@@ -0,0 +1,12 @@
+/**********************************************************************/
+/* Created by Geoffrey NORO                                           */
+/* Name : ascenceur.c                                                 */
+/* Date : 16/05/2018                                                  */
+/**********************************************************************/
+
+
+/**********************************************************************/
+/* Ce fichier contiendra la structure ascenceur permettant de         */
+/* modeliser un ascenceur, ainsi que les fonctions permettant de      */
+/* modeliser les actions de l'ascenceur.                              */
+/**********************************************************************/
