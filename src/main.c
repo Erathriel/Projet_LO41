a8bf31bc457e731e159685053fe16fb9a7c72589
Erathriel <geoffreynoro@orange.fr>
2018-06-10 16:42:34 +0200

    Fonction goTo

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
+
+#define NB_ETAGES 25
 
 typedef struct Ascenseur {
 	int id;
@@ -26,7 +29,16 @@ typedef struct Borne
 void goTo(Ascenseur asc){
 	if (asc.utilisable)
 	{
-		
+		if (etageDepart<etageCible && etageCible<NB_ETAGES)
+		{
+			/* code */
+		} 
+		else if (etageDepart<etageCible && etageCible<NB_ETAGES)
+		{
+			/* code */
+		} else{
+			printf("Erreur : etage cible ou depart non repertorie...\n");
+		}
 	}
 	else {
 		printf("Error : Ascenseur Inutilisable\n");
@@ -41,9 +53,9 @@ int main(int argc, char const *argv[])
 
 	asc1.id=1;
 	asc1.capacite=5;
-	asc1.etageDepart=0;
-	asc1.etageCible=0;
 	asc1.etageCourant=0;
+	asc1.etageDepart=etageCourant;
+	asc1.etageCible=0;
 	asc1.etat=0;
 	asc1.utilisable=true;
 
